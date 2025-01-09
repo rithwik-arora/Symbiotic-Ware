c8504b7db6b2caed43629685c4e609fabd9205d8
Rithwik Arora <rithwikarora@Rithwiks-MacBook-Air.local>
2025-01-09 18:24:21 +0530

    Drawing Assignments 1 completed

--- index1.js
+++ index1.js
@@ -1,5 +1,27 @@
-console.log("Task 1");
-setTimeout(() => {
-  console.log("Task 2 (asynchronous)");
-}, 1000); // Task 2 will be logged after 1 second
-console.log("Task 3");
+function drawDesign1(numInput) {
+  for (i = 0; i < 3; i++) {
+    console.log("*".repeat(numInput));
+  }
+}
+
+function drawDesign2(numInput) {
+  for (i = 1; i <= numInput; i++) {
+    console.log("*".repeat(i));
+  }
+}
+
+function drawDesign3(numInput) {
+  for (i = 1; i <= numInput; i++) {
+    console.log("*".repeat(i));
+  }
+  console.log("*".repeat(numInput + 1));
+  for (i = numInput; i > 0; i--) {
+    console.log("*".repeat(i));
+  }
+}
+
+const input = prompt("Enter Number: ");
+numInput = parseInt(input, 10);
+//drawDesign1(numInput);
+//drawDesign2(numInput);
+drawDesign3(numInput);
